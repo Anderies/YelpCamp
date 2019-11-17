e1d8897e81d7cad68af7975cb1e83efa551a1753
anderies <anderies@sprout.co.id>
2019-11-17 11:07:40 +0700

    YelpCamp Styling Campgrounds

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.set("view engine","ejs");
 var campgrounds = [
     { name : "Salmon Creek", image:"https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Pexels"},
     { name : " Granite Hill", image:"https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_340.jpg"},
+    { name : "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Ben_Frieden"},
+    { name : "Salmon Creek", image:"https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Pexels"},
+    { name : " Granite Hill", image:"https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_340.jpg"},
     { name : "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Ben_Frieden"}
 ]
 
