121747c60ae0ba7fc681f6851e8fa382623f9a49
anderies <anderies@sprout.co.id>
2019-11-17 23:17:22 +0700

    adding mongoose - YelpCamp data Persistence

--- app.js
+++ app.js
@@ -2,45 +2,80 @@ var express = require("express");
 var app = express();
 var port = 3000;
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost:27017/yelp_camp');
+
+
+// Schema SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: " Granite Hill",
+//         image: "https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_340.jpg"
+//     }, function (err, campgrounds) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED CAMPGROUND : ");
+//             console.log(campgrounds);
+//         }
+//     });
 
 // Tell Express to use Body Parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // Setting render to render ejs file
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-var campgrounds = [
-    { name : "Salmon Creek", image:"https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Pexels"},
-    { name : " Granite Hill", image:"https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_340.jpg"},
-    { name : "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Ben_Frieden"},
-    { name : "Salmon Creek", image:"https://www.photosforclass.com/download/pixabay-1846142?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Pexels"},
-    { name : " Granite Hill", image:"https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_340.jpg"},
-    { name : "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722c72d09f49c45f_960.jpg&user=Ben_Frieden"}
-]
+
 
 // Route to targeting views
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("landing");
 })
 
-app.get("/campgrounds",(req,res) =>{
-    res.render("campgrounds",{campgrounds:campgrounds});
+app.get("/campgrounds", (req, res) => {
+    // GET ALL CAMPGROUND FROM DB
+    Campground.find({},function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", { campgrounds: allCampgrounds });
+        }
+    })
+   
 })
 
-app.post("/campgrounds",(req,res)=>{
+app.post("/campgrounds", (req, res) => {
     // res.send("YOU HIT THE POST REQUEST!");
     // get data from form and add  to campgrounds array
-    var name = req.body.name; 
+    var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name,image: image}
-    campgrounds.push(newCampground)
-    res.redirect("/campgrounds");
-    // redirect back to campgrounds page
+    var newCampground = { name: name, image: image }
+
+    // Create a new campground and save to DB
+    Campground.create(newCampground,function(err,newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });    
 })
 
-app.get("/campgrounds/new",(req,res)=>{
+app.get("/campgrounds/new", (req, res) => {
     res.render("new.ejs")
 })
 
-app.listen(port,process.env.IP,()=>{
+app.listen(port, process.env.IP, () => {
     console.log("YELP CAMP SERVER HAS STARTED !")
 })
